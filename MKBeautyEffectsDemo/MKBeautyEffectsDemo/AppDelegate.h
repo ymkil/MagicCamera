b0f607e4babb39069d3948ac942d52b9e9d0fb10
liningkang <liningkang@zds-t.com>
2019-08-24 18:56:00 +0800

    commit
    美颜效果

---
+++ MKBeautyEffectsDemo/MKBeautyEffectsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKBeautyEffectsDemo
+//
+//  Created by mkil on 2019/8/15.
+//  Copyright © 2019 黎宁康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MKBeautyEffectsDemo/MKBeautyEffectsDemo/MKBeautyEffects/MKGPUImage/MKGPUImageContext.h
@@ -0,0 +1,53 @@
+//
+//  MKGPUImageContext.h
+//  MKBeautyEffectsDemo
+//
+//  Created by mkil on 2019/8/17.
+//  Copyright © 2019 黎宁康. All rights reserved.
+//
+
+#import <OpenGLES/EAGLDrawable.h>
+#import <AVFoundation/AVFoundation.h>
+
+@class MKGLProgram;
+@class MKGPUImageFramebuffer;
+
+#import "MKGPUImageFramebufferCache.h"
+#import "MKGPUImageConstants.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class MKGPUImageContext;
+void runAYSynchronouslyOnContextQueue(MKGPUImageContext *context, void (^block)(void));
+
+@interface MKGPUImageContext : NSObject
+
+@property(readonly, nonatomic) dispatch_queue_t contextQueue;
+@property(readonly, nonatomic) void *contextKey;
+
+@property(readonly, retain, nonatomic) EAGLContext *context;
+
+@property(readonly) CVOpenGLESTextureCacheRef coreVideoTextureCache;
+@property(readonly, retain, nonatomic) MKGPUImageFramebufferCache *framebufferCache;
+
+- (instancetype)initWithNewGLContext;
+
+- (instancetype)initWithCurrentGLContext;
+
+- (void)useAsCurrentContext;
+
+- (void)presentBufferForDisplay;
+
+- (MKGLProgram *)programForVertexShaderString:(NSString *)vertexShaderString fragmentShaderString:(NSString *)fragmentShaderString;
+
+@end
+
+@protocol MKGPUImageInput <NSObject>
+
+- (void)setInputSize:(CGSize)newSize;
+- (void)setInputFramebuffer:(MKGPUImageFramebuffer *)newInputFramebuffer;
+- (void)newFrameReady;
+
+@end
+
+NS_ASSUME_NONNULL_END
